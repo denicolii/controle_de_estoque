3301f4951c26e301e8184ecb981487178df52111
denicolii <110945232+denicolii@users.noreply.github.com>
2023-10-23 17:20:17 -0300

    atualizações-denicoli

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function logar() {
     var login = document.getElementById('login').value;
     var senha = document.getElementById('senha').value;
 
-    if (login == 'denicoli' && senha == 'delicia') {
+    if (login == 'usuario' && senha == 'usuario') {
         location.href = "estoque.html";
     } else {
         alert('Usuário ou senha incorretos.Tente novamente!');
